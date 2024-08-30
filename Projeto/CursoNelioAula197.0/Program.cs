3220f26b4759b8da01f5d0de32ff4f9f4cd2d263
ivanostorari <programadordev41@gmail.com>
2024-08-30 14:20:33 -0400

    aula197

---
+++ Projeto/CursoNelioAula197.0/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace Projeto_Xadrez_Curso_Nelio_Aula1970
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\ivan.ostorari\Desktop\C# COMPLETO PROGRAMAÇÃO ORIENTADO A OBJETOS + PROJETOS NÉLIO ALVES\PROJETOS\Projetos\Projeto_Xadrez_Curso_Nelio_Aula197.0\temp\file1.txt";
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
