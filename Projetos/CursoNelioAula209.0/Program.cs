7af5d8fb8d1e119caee4f5afb9c66a5c740b2b81
ivanostorari <programadordev41@gmail.com>
2024-09-04 16:12:07 -0400

    aula209

---
+++ Projetos/CursoNelioAula209.0/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Globalization;
+
+namespace CursoNelioAula2090
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
