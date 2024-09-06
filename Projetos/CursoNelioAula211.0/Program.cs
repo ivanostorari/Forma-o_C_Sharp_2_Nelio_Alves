e392bc719f321570860be884d36a5217f7912402
ivanostorari <programadordev41@gmail.com>
2024-09-06 16:56:15 -0400

    CursoNelioAula2110

---
+++ Projetos/CursoNelioAula211.0/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+namespace CursoNelioAula2110
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"c :\Temp\int.txt";
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    List<string> list = new List<string>();
+                    while (!sr.EndOfStream)
+                    {
+                        list.Add(sr.ReadLine());
+                    }
+                    list.Sort();
+                    foreach (string str in list)
+                    {
+                        Console.WriteLine(str);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
