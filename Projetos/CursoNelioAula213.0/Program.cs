f46db3f6246dbdf983ff05d567bd6fd3a82f4dda
ivanostorari <programadordev41@gmail.com>
2024-09-11 16:51:23 -0400

    CursoNelioAula2130

---
+++ Projetos/CursoNelioAula213.0/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CursoNelioAula2130
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintService printService = new PrintService();
+            
+            Console.Write("How many values? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i=0; i<n; i++) 
+            {
+                int x = int.Parse(Console.ReadLine());
+                printService.AddValue(x);
+            }
+            printService.Print();
+            Console.WriteLine("Fist: " + printService.Fist());
+        }
+    }
+}
