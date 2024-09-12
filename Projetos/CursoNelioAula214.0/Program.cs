7f113527e74e2e9ddc6ef290101cbda0697f6416
ivanostorari <programadordev41@gmail.com>
2024-09-12 14:03:42 -0400

    CursoNelioAula2140

--- Projetos/CursoNelioAula214.0/Program.cs
+++ Projetos/CursoNelioAula214.0/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            PrintServiceString printService = new PrintServiceString();
+
+            Console.Write("How many values? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i=0; i<n; i++)
+            {
+                string x = Console.ReadLine();
+                printService.AddValue(x);
+            }
+            printService.Print();
+            Console.WriteLine("Fist: " + printService.Fist());
         }
     }
 }
--- Projetos/CursoNelioAula214.0/obj/Debug/net6.0/CursoNelioAula214.0.AssemblyInfo.cs
+++ Projetos/CursoNelioAula214.0/obj/Debug/net6.0/CursoNelioAula214.0.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CursoNelioAula214.0")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f46db3f6246dbdf983ff05d567bd6fd3a82f4dda")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3135ecac0d514257893569b24b65a3e980c6a777")]
 [assembly: System.Reflection.AssemblyProductAttribute("CursoNelioAula214.0")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CursoNelioAula214.0")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
